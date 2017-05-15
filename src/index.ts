ef49532776ed164c16c36fb93c350d8f1b0984db
Victor Fernandez de Alba <sneridagh@gmail.com>
2017-05-15 13:51:34 +0200

    Fix AoT compilation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MediumEditorComponent } from './medium.editor.component';
 
+export * from './medium.editor.component';
 
 @NgModule({
   imports: [
